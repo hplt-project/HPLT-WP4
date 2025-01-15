afca9a34a96e2a1bed66a83df2773b4446383da0
MariaFjodorowa <laks.salmon@gmail.com>
2025-01-16 00:36:26 +0100

    sanity longer sample

--- encoder-only/sanity_check.py
+++ encoder-only/sanity_check.py
@@ -9,10 +9,14 @@ if __name__ == '__main__':
     current_model_path = os.path.join(models_path, lang)
     tokenizer = AutoTokenizer.from_pretrained(current_model_path)
     model = AutoModelForMaskedLM.from_pretrained(current_model_path, trust_remote_code=True)
-    samples = ("2024 застаўся ў [MASK]...", "І 366 дзён мы былі максімальна [MASK].", "І 366 дзён [MASK] былі максімальна сур'ёзныя")
+    samples = ("""
+    82-я цырымонія ўручэння прэміі «Залаты глобус» [MASK] цырымонія ўзнагароджання [MASK] заслугі [MASK] галіне кінематографа [MASK] тэлебачання за 2024 [MASK],
+     якая адбылася 5 студзеня 2025 года. Транслявалася каналам CBS. Вядучай цырымоніі была комік Нікі Глейзер.
+    """) # https://be.wikipedia.org/wiki/%D0%97%D0%B0%D0%BB%D0%B0%D1%82%D1%8B_%D0%B3%D0%BB%D0%BE%D0%B1%D1%83%D1%81_(%D0%BF%D1%80%D1%8D%D0%BC%D1%96%D1%8F,_2025)
     mask_id = tokenizer.convert_tokens_to_ids("[MASK]")
     for sample in samples:
         input_text = tokenizer(sample, return_tensors="pt")
+        print(input_text.input_ids)
         output_p = model(**input_text)
         output_text = torch.where(input_text.input_ids == mask_id, output_p.logits.argmax(-1), input_text.input_ids)
 
