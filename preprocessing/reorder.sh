2c87d2cbc5ecde5de0c4aebbce367490bc59f8f7
MariaFjodorowa <laks.salmon@gmail.com>
2025-01-16 01:03:07 +0100

    reorder

--- preprocessing/reorder.sh
+++ preprocessing/reorder.sh
@@ -19,5 +19,5 @@ export EBU_USER_PREFIX=/projappl/project_465001384/software/
 # the important bit: unload all current modules (just in case) and load only the necessary ones
 module --quiet purge
 module load LUMI PyTorch/2.2.2-rocm-5.6.1-python-3.10-vllm-0.4.0.post1-singularity-20240617
-OTHER_ARGS=${@}
-srun singularity exec $SIF python3 reorder.py ${OTHER_ARGS}
+FOLDERS=${1}
+srun singularity exec $SIF python3 reorder.py --problematic_folders ${FOLDERS}
