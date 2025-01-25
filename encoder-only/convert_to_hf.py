853723bc08e01767636f7edfcde7573068ea0b9b
MariaFjodorowa <laks.salmon@gmail.com>
2025-01-25 05:54:27 +0100

    fix -path

--- encoder-only/convert_to_hf.py
+++ encoder-only/convert_to_hf.py
@@ -3,6 +3,7 @@ import torch
 import argparse
 import json
 
+from selenium.webdriver.common.devtools.v85.debugger import step_out
 from transformers import AutoTokenizer
 from train import FREQUENT_CHECKPOINTING, FREQUENT_CHECKPOINTING_STEPS
 
@@ -22,7 +23,7 @@ def convert_to_hf(input_model_directory, output_model_directory, language):
         checkpointing_steps += list(FREQUENT_CHECKPOINTING_STEPS) + [0]
     for step in checkpointing_steps:
         checkpoints_directory = os.path.join(input_model_directory, f"{language}/hplt_models/bert_base_{language}")
-        output_model_directory = os.path.join(output_model_directory, language+f'_{step}')
+        step_output_model_directory = os.path.join(output_model_directory, language+f'_{step}')
         checkpoint_path = os.path.join(
             checkpoints_directory, f"model_step_{step}.bin",
         )
@@ -31,28 +32,28 @@ def convert_to_hf(input_model_directory, output_model_directory, language):
         if not os.path.exists(checkpoint_path):
             raise ValueError(f"Model file {checkpoint_path} does not exist")
 
-        if not os.path.exists(output_model_directory):
-            os.makedirs(output_model_directory)
+        if not os.path.exists(step_output_model_directory):
+            os.makedirs(step_output_model_directory)
 
         prototype_directory = "huggingface_prototype"
-        os.system(f"cp {prototype_directory}/* {output_model_directory}")
+        os.system(f"cp {prototype_directory}/* {step_output_model_directory}")
 
         if torch.cuda.is_available():
             checkpoint = torch.load(checkpoint_path)
         else:
             checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
-        torch.save(checkpoint['model'], os.path.join(output_model_directory, "pytorch_model.bin"))
+        torch.save(checkpoint['model'], os.path.join(step_output_model_directory, "pytorch_model.bin"))
 
-        os.system(f"cp {input_model_directory}/{language}/tokenizer.json {output_model_directory}")
+        os.system(f"cp {input_model_directory}/{language}/tokenizer.json {step_output_model_directory}")
 
-        tokenizer = AutoTokenizer.from_pretrained(output_model_directory)
+        tokenizer = AutoTokenizer.from_pretrained(step_output_model_directory)
         vocabulary_size = tokenizer.vocab_size
-        config_dict = json.load(open(os.path.join(output_model_directory, "config.json")))
+        config_dict = json.load(open(os.path.join(step_output_model_directory, "config.json")))
         config_dict["vocab_size"] = vocabulary_size
         json.dump(
             config_dict,
-            open(os.path.join(output_model_directory, "config.json"), "w"),
-            indent=4
+            open(os.path.join(step_output_model_directory, "config.json"), "w"),
+            indent=4,
         )
 
 
