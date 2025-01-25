f66b08c3d1c41a3f8e69196717e9904f34bb67d0
MariaFjodorowa <laks.salmon@gmail.com>
2025-01-25 05:15:49 +0100

    convert frequent checkpoints

--- encoder-only/convert_to_hf.py
+++ encoder-only/convert_to_hf.py
@@ -3,8 +3,8 @@ import torch
 import argparse
 import json
 
-from torch.utils.checkpoint import checkpoint
 from transformers import AutoTokenizer
+from train import FREQUENT_CHECKPOINTING, FREQUENT_CHECKPOINTING_STEPS
 
 
 def parse_args():
@@ -17,38 +17,43 @@ def parse_args():
 
 
 def convert_to_hf(input_model_directory, output_model_directory, language):
-    checkpoints_directory = os.path.join(input_model_directory, f"{language}/hplt_models/bert_base_{language}")
-    output_model_directory = os.path.join(output_model_directory, language)
+    checkpointing_steps = [31250]
+    if language in FREQUENT_CHECKPOINTING:
+        checkpointing_steps += list(FREQUENT_CHECKPOINTING_STEPS) + [0]
+    for step in checkpointing_steps:
+        checkpoints_directory = os.path.join(input_model_directory, f"{language}/hplt_models/bert_base_{language}")
+        output_model_directory = os.path.join(output_model_directory, language+f'_{step}')
+        checkpoint_path = os.path.join(
+            checkpoints_directory, f"model_step_{step}.bin",
+        )
+        if not os.path.exists(checkpoints_directory):
+            raise ValueError(f"Model directory {checkpoints_directory} does not exist")
+        if not os.path.exists(checkpoint_path):
+            raise ValueError(f"Model file {checkpoint_path} does not exist")
 
-    if not os.path.exists(checkpoints_directory):
-        raise ValueError(f"Model directory {checkpoints_directory} does not exist")
-    if not os.path.exists(os.path.join(checkpoints_directory, "model_step_31250.bin")):
-        raise ValueError(f"Model file {os.path.join(checkpoints_directory, 'model_step_31250.bin')} does not exist")
+        if not os.path.exists(output_model_directory):
+            os.makedirs(output_model_directory)
 
-    if not os.path.exists(output_model_directory):
-        os.makedirs(output_model_directory)
+        prototype_directory = "huggingface_prototype"
+        os.system(f"cp {prototype_directory}/* {output_model_directory}")
 
-    prototype_directory = "huggingface_prototype"
-    os.system(f"cp {prototype_directory}/* {output_model_directory}")
+        if torch.cuda.is_available():
+            checkpoint = torch.load(checkpoint_path)
+        else:
+            checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+        torch.save(checkpoint['model'], os.path.join(output_model_directory, "pytorch_model.bin"))
 
-    last_checkpoint_name = os.path.join(checkpoints_directory, "model_step_31250.bin")
-    if torch.cuda.is_available():
-        checkpoint = torch.load(last_checkpoint_name)
-    else:
-        checkpoint = torch.load(last_checkpoint_name, map_location=torch.device('cpu'))
-    torch.save(checkpoint['model'], os.path.join(output_model_directory, "pytorch_model.bin"))
+        os.system(f"cp {input_model_directory}/{language}/tokenizer.json {output_model_directory}")
 
-    os.system(f"cp {input_model_directory}/{language}/tokenizer.json {output_model_directory}")
-
-    tokenizer = AutoTokenizer.from_pretrained(output_model_directory)
-    vocabulary_size = tokenizer.vocab_size
-    config_dict = json.load(open(os.path.join(output_model_directory, "config.json")))
-    config_dict["vocab_size"] = vocabulary_size
-    json.dump(
-        config_dict,
-        open(os.path.join(output_model_directory, "config.json"), "w"),
-        indent=4
-    )
+        tokenizer = AutoTokenizer.from_pretrained(output_model_directory)
+        vocabulary_size = tokenizer.vocab_size
+        config_dict = json.load(open(os.path.join(output_model_directory, "config.json")))
+        config_dict["vocab_size"] = vocabulary_size
+        json.dump(
+            config_dict,
+            open(os.path.join(output_model_directory, "config.json"), "w"),
+            indent=4
+        )
 
 
 def main():
