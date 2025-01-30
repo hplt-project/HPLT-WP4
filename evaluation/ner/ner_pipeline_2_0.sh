47249bccbd7367228ad684ef78cfd96c69e06c87
MariaFjodorowa <laks.salmon@gmail.com>
2025-01-30 12:22:38 +0100

    py instead of sh

--- evaluation/ner/ner_pipeline_2_0.sh
+++
@@ -1,9 +0,0 @@
-for lang in /scratch/project_465001386/hplt-2-0-output/hplt_hf_models/*
-  do
-    lang_only=$(basename "$lang")
-    echo $lang_only
-    if [ ! -f  /scratch/project_465001386/hplt-2-0-output/logs/ner_${lang_only}/ner_results.tsv ]; then
-      echo "Running ${lang_only}"
-      sbatch -J ner-${lang_only} run_ner.slurm /scratch/project_465001386/hplt_hf_models/${lang_only}/ wikiann/${lang_only} /scratch/project_465001386/hplt-2-0-output/logs/ner_${lang_only}
-    fi
-  done
