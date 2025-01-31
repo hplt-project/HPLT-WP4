f7510dc99cd32c4418ed05baada6df444e1eda84
MariaFjodorowa <laks.salmon@gmail.com>
2025-01-31 10:33:15 +0100

    curl for albanian ud

---
+++ evaluation/ud/download_2_15.sh
@@ -0,0 +1,3 @@
+cd /scratch/project_465001386
+curl --remote-name-all https://lindat.mff.cuni.cz/repository/xmlui/bitstream/handle/11234/1-5787/ud-treebanks-v2.15.tgz
+tar -xzf ud-treebanks-v2.15.tgz
