401667e04197ac68d9b11dd986773bdc4e6dc416
MariaFjodorowa <laks.salmon@gmail.com>
2025-03-19 13:08:33 +0100

    easier readme update

--- utils/update_readme.py
+++ utils/update_readme.py
@@ -1,6 +1,7 @@
 from huggingface_hub import HfApi, hf_hub_download
 from constants import LANGS_MAPPING
 
+
 if __name__ == '__main__':
     api = HfApi()
     for short, long in LANGS_MAPPING.items():
@@ -8,11 +9,13 @@ if __name__ == '__main__':
         problem_repo = f'HPLT/hplt_bert_base_2_0_{long}'
         readme_path_in_repo = "README.md"
         main_readme_path = hf_hub_download(repo_id=problem_repo, filename=readme_path_in_repo)
-        bad = "## Example usage"
+        with open('before.txt', 'r') as f:
+            before = f.read()
+        with open('after.txt', 'r') as f:
+            after = f.read()
         with open(main_readme_path, 'r') as file:
             contents = file.read()
-        contents = contents.replace(bad, "## Example usage (tested with `transformers==4.46.1` and `tokenizers==0.20.1`)")
-        contents = contents.replace("tokenizer.decode(output_text[0].tolist())", "tokenizer.decode(output_text[0].tolist(), clean_up_tokenization_spaces=True)")
+        contents = contents.replace(before, after)
         with open(main_readme_path, 'w') as file:
             file.write(contents)
         refs = api.list_repo_refs(problem_repo)
@@ -22,6 +25,6 @@ if __name__ == '__main__':
                 path_in_repo=readme_path_in_repo,
                 repo_id=problem_repo,
                 repo_type="model",
-                commit_message=f"Updating README with clean_up_tokenization_spaces",
+                commit_message=f"Updating README",
                 revision=branch.name,
             )
