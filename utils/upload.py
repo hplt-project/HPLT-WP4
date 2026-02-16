be688cb15f351818f102161c88e71c3a128fbcf4
MariaFjodorowa <laks.salmon@gmail.com>
2026-02-16 18:16:48 +0100

    add to collection

--- utils/upload.py
+++ utils/upload.py
@@ -11,5 +11,12 @@ except HfHubHTTPError as e:
     print(e, flush=True)
 api = HfApi()
 
+lang = sys.argv[3]
 api.upload_folder(folder_path=sys.argv[2], repo_id=sys.argv[1], repo_type="model")
-
+arch="gpt_bert"
+api.add_collection_item(
+                collection_slug="HPLT/hplt-30-gpt-bert-models",
+                item_id=f"HPLT/hplt_{arch}_base_3_0_{lang}", 
+                item_type="model",
+                exists_ok=True,
+                )
