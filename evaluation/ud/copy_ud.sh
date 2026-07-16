4749f45b83943da19b537ec4faf133b39123fbb4
MariaFjodorowa <laks.salmon@gmail.com>
2026-07-16 10:35:38 +0200

    checkpoint paths

--- evaluation/ud/copy_ud.sh
+++ evaluation/ud/copy_ud.sh
@@ -1,8 +1,10 @@
-for checkpoint in /cluster/work/projects/nn9851k/mariiaf/hplt/ud_checkpoints/*.bin
+checkpoints_path=${1}
+out_path=${2}
+for checkpoint in $checkpoints_path/*.bin
     do
         echo $checkpoint
         lang=$(awk -v var="$checkpoint" 'BEGIN { print substr(var,length(var)-16, 8) }')
         echo  $lang
-        cp -r huggingface_prototype/ /cluster/work/projects/nn9851k/mariiaf/hplt_ud_hf/$lang/
-        cp $checkpoint /cluster/work/projects/nn9851k/mariiaf/hplt_ud_hf/$lang/model.bin
+        cp -r huggingface_prototype/ $out_path/$lang/
+        cp $checkpoint $out_path/$lang/model.bin
     done
